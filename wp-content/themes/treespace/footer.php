fe79b1527b4972c52dd5f2f8464c715b23b75dd8
Sangwoo-Oh <sangwoooh1013@gmail.com>
2023-03-05 14:52:43 +0900

    フッターリンク非公開

--- wp-content/themes/treespace/footer.php
+++ wp-content/themes/treespace/footer.php
@@ -1,8 +1,8 @@
 <footer class="footer">
-    <ul class="footer__menu">
+    <!-- <ul class="footer__menu">
         <li><a href="#">サイトポリシー</a></li>
         <li><a href="#">お問い合わせ</a></li>
-    </ul>
+    </ul> -->
     <p class="copyright">© Copyright TreeSpace</p>
 </footer>
 <?php wp_footer(); ?>
